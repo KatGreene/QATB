cf95194b6aa684bb019be5f4575effa6889ca2fb
KatGreene <CGZ_catgreen1234@outlook.com>
2024-03-14 19:50:53 +0800

    Added Titles

---
+++ addon/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+#    <QATB, Blender addon for quick adding time baking.>
+#    Copyright (C) <2024> <绿毛猫KatGreene>
+
--- addon/classes/core.py
+++ addon/classes/core.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+
+#    <QATB, Blender addon for quick adding time baking.>
+#    Copyright (C) <2024> <绿毛猫KatGreene>
+
+
 from typing import Set
 import bpy
 from math import log, exp
